dc5cfc89c5d08b1f858d7055036f829e8797b279
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-25 07:35:13 +0100

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+  function:
+    minOperations
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+      method that calculates the fewest number of operations needed
+      to result in exactly n H characters in the file.
+    """
+    if n == 1:
+        return 0
+
+    for i in range(2, n + 1):
+        if n % i == 0:
+            m = int(n / i)
+            return i + minOperations(m)

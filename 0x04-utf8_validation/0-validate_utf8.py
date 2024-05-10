991ebcfe85d77b39717959cc5a467e32c6104e68
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-10 11:48:25 +0100

    improve code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,55 +12,37 @@ def validUTF8(data: List) -> bool:
         represents a valid UTF-8 encoding
     '''
     binary_data = []
-    num_bytes = 0
     is_valid = True
-
-    for element in data:
-        binary = bin(element)[2:]
-        if len(binary) <= 8:
+    for num in data:
+        binary = bin(num)[2:]
+        if len(binary) < 8:
             binary = '0' * (8 - len(binary)) + binary
-        else:
+        elif len(binary) > 8:
             binary = binary[-8:]
         binary_data.append(binary)
 
-    for j in range(len(binary_data)):
-        for k in binary_data[j:j + num_bytes]:
-            if k[0] != '1' or k[1] != '0':
-                is_valid = False
-                break
-            else:
-                continue
-
-        if is_valid is False:
-            break
-
-        if j + num_bytes < len(binary_data):
-            j = j + num_bytes
-            num_bytes = 0
-        else:
-            break
-
-        if binary_data[j].startswith("0"):
-            continue
-        elif binary_data[j].startswith("110"):
-            if j == len(binary_data) - 1:
-                is_valid = False
-                break
+    i = 0
+    while i < len(binary_data):
+        num_bytes = 0
+        byte = binary_data[i]
+        if byte.startswith('0'):
             num_bytes = 1
-            continue
-        elif binary_data[j].startswith("1110"):
-            if j == len(binary_data) - 1:
-                is_valid = False
-                break
+        elif byte.startswith('110'):
             num_bytes = 2
-            continue
-        elif binary_data[j].startswith("11110"):
-            if j == len(binary_data) - 1:
-                is_valid = False
-                break
+        elif byte.startswith('1110'):
             num_bytes = 3
-            continue
+        elif byte.startswith('11110'):
+            num_bytes = 4
         else:
             is_valid = False
             break
+
+        for j in range(i + 1, i + num_bytes):
+            if j >= len(binary_data) or not binary_data[j].startswith('10'):
+                is_valid = False
+                break
+
+        if is_valid is False:
+            return is_valid
+        i += num_bytes
     return is_valid

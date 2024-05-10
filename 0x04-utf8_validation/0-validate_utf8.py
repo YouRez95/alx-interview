5a9c92dc46a5aae8aae4600438c35679e5c9e5d5
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-10 11:22:10 +0100

    improve the readability of code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,17 +12,17 @@ def validUTF8(data: List) -> bool:
         represents a valid UTF-8 encoding
     '''
     binary_data = []
+    num_bytes = 0
+    is_valid = True
+
     for element in data:
         binary = bin(element)[2:]
         if len(binary) <= 8:
-            for i in range(len(binary), 8):
-                binary = '0' + binary
-            # binary = '0' * (8 - len(binary)) + binary
+            binary = '0' * (8 - len(binary)) + binary
         else:
             binary = binary[-8:]
         binary_data.append(binary)
-    num_bytes = 0
-    is_valid = True
+
     for j in range(len(binary_data)):
         for k in binary_data[j:j + num_bytes]:
             if k[0] != '1' or k[1] != '0':
@@ -40,25 +40,21 @@ def validUTF8(data: List) -> bool:
         else:
             break
 
-        if binary_data[j][0] == '0':
+        if binary_data[j].startswith("0"):
             continue
-        elif (binary_data[j][0] == '1' and binary_data[j][1] == '1' and
-              binary_data[j][2] == '0'):
+        elif binary_data[j].startswith("110"):
             if j == len(binary_data) - 1:
                 is_valid = False
                 break
             num_bytes = 1
             continue
-        elif (binary_data[j][0] == '1' and binary_data[j][1] == '1' and
-              binary_data[j][2] == '1' and binary_data[j][3] == '0'):
+        elif binary_data[j].startswith("1110"):
             if j == len(binary_data) - 1:
                 is_valid = False
                 break
             num_bytes = 2
             continue
-        elif (binary_data[j][0] == '1' and binary_data[j][1] == '1' and
-              binary_data[j][2] == '1' and binary_data[j][3] == '1' and
-              binary_data[j][4] == '0'):
+        elif binary_data[j].startswith("11110"):
             if j == len(binary_data) - 1:
                 is_valid = False
                 break

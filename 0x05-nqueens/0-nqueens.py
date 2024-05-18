bb57e2a97eaf9638cbe17e21d61f93cc17995203
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-18 14:04:47 +0100

    improve the code and fix some bugs

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,85 +1,67 @@
 #!/usr/bin/python3
+
 '''
-    Task:
-        n queens
+    nqueens problem: (backtracking)
+        find_solution(board, row, solutions):
+            check if we reach the end of board
+                true -> print solutions
+                false -> continue
+            Iterate throgh each row and run find_soltions
+            recursively one function for each row:
+                if we find a valid place append the place to the solutions
+                else means we dont find any place for that specific row
+                    pop the last soltions and continue to find another place
 '''
+
 
 import sys
-import math
-
-sys.setrecursionlimit(5000)
-
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
-
-board = sys.argv[1]
-
-try:
-    board = int(board)
-except ValueError:
-    print('N must be a number')
-    exit(1)
-
-if board < 4:
-    print('N must be at least 4')
-    exit(1)
 
 
-def find_solutions(row, col, solutions):
-
+def find_solutions(board, row, solutions) -> None:
     '''
-        n-queens problem
+        board: num of board
+        row: start with 0
+        solutions: list of solutions
     '''
     if row == board:
+        print(solutions)
         return
-
-    new_place = valid_place(row, col, solutions)
-    # print("valid: ", new_place)
-    if new_place:
-        solutions.append(new_place)
-        # print("add", solutions)
-        if len(solutions) == board:
-            print(solutions)
-            last_path = solutions.pop()
-            find_solutions(last_path[0], last_path[1] + 1, solutions)
-        else:
-            find_solutions(row + 1, 0, solutions)
-    else:
-        if len(solutions) == 0:
-            return
-        last_path = solutions.pop()
-        # print("remove", solutions)
-        find_solutions(last_path[0], last_path[1] + 1, solutions)
+    print(solutions)
+    for col in range(board):
+        if is_valid(row, col, solutions):
+            solutions.append([row, col])
+            find_solutions(board, row + 1, solutions)
+            solutions.pop()
 
 
-def valid_place(row, col, solutions):
+def is_valid(row, col, solutions) -> bool:
     '''
-        return the valid place
-        else False
+        check the validity of place:
+            [0, 1] and [1, 2] is a wrong place for the second item
+                checking for the diagonale is abs(0 - 1) == abs(1 - 2)
+            [0, 0] and [1, 0] is a wrong place is align verticaly
+                checking is compare each previous col by current col
     '''
-    if col == board:
-        return False
-
-    if is_valid(row, col, solutions):
-        return [row, col]
-
-    result = valid_place(row, col + 1, solutions)
-    if result is not False:
-        return result
-
-    return False
-
-
-def is_valid(row, col, solutions):
-    '''
-        check if is valid place
-    '''
-    for path in solutions:
-        if path[1] == col or abs(path[0] - row) == abs(path[1] - col):
+    for r, c in solutions:
+        if col == c or abs(r - row) == abs(c - col):
             return False
     return True
 
 
-solutions = []
-find_solutions(0, 0, solutions)
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
+
+    try:
+        board = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
+    if board < 4:
+        print('N must be at least 4')
+        exit(1)
+
+    solutions = []
+    find_solutions(board, 0, solutions)

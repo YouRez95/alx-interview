6ecda418fb47d90011478f2b48f09284f7e51eb0
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-24 00:01:30 +0100

    star-wars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/Users/mac/.nvm/versions/node/v21.6.2/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+function load (character) {
+  return new Promise((resolve, reject) => {
+    request(character, function (error, response, body) {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(JSON.parse(body).name);
+    });
+  });
+}
+
+request(url, async function (error, response, body) {
+  if (error) {
+    throw error;
+  }
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    try {
+      const name = await load(character);
+      console.log(name);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});

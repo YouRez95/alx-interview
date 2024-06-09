9ddb1afac834fc8c31a136e8e5ecb9285c2dd1ba
Youness Elalouani <younesselalouani1995@gmail.com>
2024-06-09 17:00:18 +0100

    making change problem with greedy algorithm

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,40 +7,15 @@
 '''
 
 
-def count(coins, sum, total, result):
-    '''
-        recursive function will called until meet the total
-        or the coins is empty list
-    '''
-    if len(coins) == 0:
-        return
-    largest_num = coins[-1]
-    if sum + largest_num < total:
-        sum += largest_num
-        result.append(largest_num)
-    elif sum + largest_num > total:
-        coins.pop()
-    else:
-        sum += largest_num
-        result.append(largest_num)
-        return result
-    count(coins, sum, total, result)
-
-
 def makeChange(coins, total):
-    '''
-        entry of the problem
-        will check the result if has sum is equal total
-        then we return the len(result) which is the total of coins
-        else -1
-    '''
     if total <= 0:
         return 0
-    if len(coins) == 0:
-        return 0
-    start_sum = 0
-    result = []
-    coins.sort()
-    count(coins, start_sum, total, result)
-    final_sum = sum(result)
-    return len(result) if final_sum == total else -1
+
+    max_value = total + 1
+    result = [max_value] * (total + 1)
+    result[0] = 0
+    for coin in coins:
+        for x in range(coin, total + 1):
+            result[x] = min(result[x], result[x - coin] + 1)
+
+    return result[total] if result[total] != max_value else -1

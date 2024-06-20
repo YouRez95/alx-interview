36dfd86837853541e86283d77a823f23cd3d1d45
Youness Elalouani <younesselalouani1995@gmail.com>
2024-06-20 22:31:44 +0100

    improve efficiently

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -15,11 +15,7 @@ def isPrime(n):
     '''
     if n == 1:
         return False
-    if n == 2:
-        return True
-    if n > 2 and n % 2 == 0:
-        return False
-    for i in range(3, n, 2):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
